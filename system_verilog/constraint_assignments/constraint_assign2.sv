aa67176e5bd8680de17ec3419580a03c9c44d8a4
LanwinDsouza <104729279+LanwinDsouza@users.noreply.github.com>
2024-06-17 10:46:24 +0530

    Update constraint_assign2.sv

--- system_verilog/constraint_assignments/constraint_assign2.sv
+++ system_verilog/constraint_assignments/constraint_assign2.sv
@@ -9,16 +9,29 @@ Description: Concept of Constraint Randomization in System Verilog
 class constraint_assign2;
   
 //ADD_CODE: Declare the dynamic array as address of size 10. 
+  rand int address [];
+  constraint c_{address.size == 10;}
+  
+  
 //ADD_CODE: Write constraint for a dynamic array address of size 10 to generate even numbers.
+  constraint c__{foreach(address[i]) address[i][0]==1'b0 && address[i] inside{[0:100]};}
   
 endclass:constraint_assign2 
+
 module con_assign2; 
   //ADD_CODE: Declare the handle for "class constraint_assign2" as c0.
+  constraint_assign2 c0;
 initial
   begin
     //ADD_CODE: Create the Object for  handle
+    c0=new;
     //ADD_CODE: Randomize the object for genrating randomize values for address with even numbers.
+    c0.randomize;
+    $display("####Generating %0d even random variables ...............",c0.address.size);
+    $write("-->");
     //ADD_CODE: Display the values of of array address using object handel.
+    $display("%p",c0.address);
         
   end 
-endmodule:con_assign2
+endmodule:con_assign2	
+	
