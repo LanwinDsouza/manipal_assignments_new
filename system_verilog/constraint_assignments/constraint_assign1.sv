8209bb7eb66a04643ae5e8fbbb71b340c648a6c2
LanwinDsouza <104729279+LanwinDsouza@users.noreply.github.com>
2024-06-17 10:45:17 +0530

    Update constraint_assign1.sv

--- system_verilog/constraint_assignments/constraint_assign1.sv
+++ system_verilog/constraint_assignments/constraint_assign1.sv
@@ -6,19 +6,32 @@ Date:   	7th June 2024
 Version:	1.0
 Description: Concept of Constraint Randomization in System Verilog 
 ***************************************************************************/
+`define REP 10
+
 class constraint_assign1;
   
 //ADD_CODE: Declare the 8bit variable as data. 
+  rand bit [7:0] data;
 //ADD_CODE: Write constraint for an 8bit variable data to generate values divisible by 5.
-  
+  constraint c_{data%5==0 && data inside {[0:100]};} 
 endclass:constraint_assign1 
 module con_assign1; 
   //ADD_CODE: Declare the handle for "class constraint_assign1" as c0.
+  constraint_assign1 c;
 initial
   begin
     //ADD_CODE: Create the Object for  handle
+    c=new;
     //ADD_CODE: Randomize the object for genrating randomize values for data which is divisible by 5
+    $display("####Generating %0d random variables divisible by 5 ...............",`REP);
+    $write("-->");
+    repeat(`REP) begin
+    c.randomize;
     //ADD_CODE: Display the values of data using object handel.
+      $write("%d,",c.data);
         
-  end 
+    end 
+    $display("\n");
+  end
 endmodule:con_assign1
+
